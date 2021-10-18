12d78c77956dcd2dc5c224bd5fe50f9eef63ef49
root <root@DESKTOP-81U515C.localdomain>
2021-10-18 16:31:51 +1100

    made create.sh do what it is supposed to do

--- create.sh
+++ create.sh
@@ -1,5 +1,12 @@
-#! /bin/bash
+#! /bin/sh
+
+set -e
 
 : "${1:?Please pass path to file system image}"
 : "${2:?Please pass name of generated file system}"
 
+mkdir -p "sysroots/$2"
+
+TAR_PATH=$(realpath $1)
+
+cd sysroots/$2 && tar -xf $TAR_PATH
